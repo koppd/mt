389588ff35bab647a1569607cdf02f10fbfacba7
Dominik Kopp <d.kopp@koppnik.de>
2015-05-07 05:48:07 -0700

    initial GUI. only mock - no signal slot

---
+++ startgui.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+import sys
+from PySide import QtCore, QtGui
+from mainwindow import Ui_MainWindow
+
+
+
+class ControlMainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(ControlMainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    mySW = ControlMainWindow()
+    mySW.show()
+    sys.exit(app.exec_())
