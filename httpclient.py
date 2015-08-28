4431420b466a29292aa8d3a21d829a8a19d452b1
Dominik Kopp <github@koppnik.de>
2015-08-28 12:11:58 +0200

    den "File Download" (HTTP) auslager und mit mehr benutzeraktion versehen. z.B. starte erst auf ENTER und Downloadwiederholung

---
+++ httpclient.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 08:21:56 2015
+
+@author: mininet
+"""
+
+import time
+import os
+import signal
+import argparse
+import sys
+from socket import *
+import re
+from subprocess import call
+
+def call_wget(server, filename):
+    call(["wget", "-O", "/dev/null", "%s/%s]) % (server, filename)
+
+
+def startRealDownload(server, filename):
+
+    if 1==2:
+#        CLI(self.net)
+        pass
+    else:
+# Erzeuge Socket
+        raw_input("Press Enter to download file %s from server %s. ") % (server, filename)
+
+        redownload = True
+        while redownload == True:
+            call_wget(server, filename)
+            again = raw_input("Download complete. Download again? (y/N) ")
+            if not(again == "y" or again == "Y"):
+                redownload = False
+
+        raw_input("Demo is over. Press Enter to close window. ")
+
+        return 0
+
+if __name__ == '__main__':
+    #setLogLevel( 'info' )
+
+    def type_port(x):
+        x = int(x)
+        if x < 1 or x > 65535:
+            raise argparse.ArgumentTypeError(
+                "Port number has to be greater than 1 and less than 65535.")
+        return x
+
+    description = ("Download a file from a HTTP server")
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("-s",
+                        "--server",
+                        help="The IP of the http server. "
+                             "(Default: '10.0.0.1')",
+                        default="10.0.0.1")
+    parser.add_argument("-f",
+                        "--file",
+                        help="file name",
+                        type=str,
+                        required=True)
+
+    args = parser.parse_args()
+
+    exit(startRealDownload(server=args.server,
+                      filename=args.file))
